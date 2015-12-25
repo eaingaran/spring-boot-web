4893db3add46e5beaa91f529f6aec4b1c46e61f4
Marco Tedone <marco.tedone@gmail.com>
2015-12-25 19:13:40 +0000

    Added webjars for boostrap and jquery and configured Spring security to let them through

--- src/main/java/uk/co/jemos/spring/boot/thymeleaf/WebSecurityConfig.java
+++ src/main/java/uk/co/jemos/spring/boot/thymeleaf/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/webjars/**").permitAll();
         http
                 .authorizeRequests()
                     .antMatchers("/").permitAll()
