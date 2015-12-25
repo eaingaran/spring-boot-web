3cfd259565309f88747ccc16b883cbbab6d20793
Marco Tedone <marco.tedone@gmail.com>
2015-12-25 17:29:35 +0000

    Created boot web app with thymeleaf, bootstrap and jquery support

---
+++ src/main/java/uk/co/jemos/spring/boot/thymeleaf/CoreController.java
@@ -0,0 +1,16 @@
+package uk.co.jemos.spring.boot.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tedonema on 25/12/2015.
+ */
+@Controller
+public class CoreController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
